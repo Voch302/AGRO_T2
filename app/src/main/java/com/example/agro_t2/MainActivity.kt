5d7110669de468046d57060fe9edfe6c382bfb58
Alex <Alekseevnikita886@gmail.com>
2024-10-23 09:46:16 +0300

    alex

--- app/src/main/java/com/example/agro_t2/MainActivity.kt
+++ app/src/main/java/com/example/agro_t2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.agro_t2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val user_email:EditText = findViewById(R.id.user_email_reg)
+        val user_login:EditText = findViewById(R.id.user_login_reg)
+        val user_pass:EditText = findViewById(R.id.user_pass_reg)
+        val button_reg:Button = findViewById(R.id.button_reg)
 
 
 
