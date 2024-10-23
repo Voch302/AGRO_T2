201beaf163fd9f85c38ca125f48cbd27baf52029
PurpleGlina <Chernoivanov.vx@gmail.com>
2024-10-24 00:34:55 +0300

    chat with contoller

---
+++ app/src/main/java/com/example/agro_t2/Chat_Contoller.kt
@@ -0,0 +1,18 @@
+package com.example.agro_t2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Chat_Contoller : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chat_contoller)
+
+
+        }
+    }
+}
