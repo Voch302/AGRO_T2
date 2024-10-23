dba97d2f54f67febca680f7b55474f9b13a4fce5
Alex <Alekseevnikita886@gmail.com>
2024-10-23 11:34:12 +0300

    alex3 сделан вход и регистрация с  всплывающими  подсказками

--- app/src/main/java/com/example/agro_t2/AuthActivity.kt
+++ app/src/main/java/com/example/agro_t2/AuthActivity.kt
@@ -1,20 +1,46 @@
 package com.example.agro_t2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AuthActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
 
+        val emailAuth: EditText = findViewById(R.id.user_email_auth)
+        val passAuth: EditText = findViewById(R.id.user_pass_auth)
+        val buttonAuth: Button = findViewById(R.id.button)
+        val textToReg: TextView = findViewById(R.id.textToReg_auth)
+
+        buttonAuth.setOnClickListener {
+            val email = emailAuth.text.toString()
+            val pass = passAuth.text.toString()
+
+            if (email == "" || pass == "")
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(this, "Успешный вход", Toast.LENGTH_LONG).show()
+        }
+
+
+        textToReg.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
 
     }
 }
+
--- app/src/main/java/com/example/agro_t2/MainActivity.kt
+++ app/src/main/java/com/example/agro_t2/MainActivity.kt
@@ -1,24 +1,52 @@
 package com.example.agro_t2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val user_email:EditText = findViewById(R.id.user_email_reg)
-        val user_login:EditText = findViewById(R.id.user_login_reg)
-        val user_pass:EditText = findViewById(R.id.user_pass_reg)
-        val button_reg:Button = findViewById(R.id.button_reg)
+        val userEmail: EditText = findViewById(R.id.user_email_reg)
+        val userLogin: EditText = findViewById(R.id.user_login_reg)
+        val userPass: EditText = findViewById(R.id.user_pass_reg)
+        val buttonReg: Button = findViewById(R.id.button_reg)
+        val textToAuth: TextView = findViewById(R.id.textToAuth_reg)
 
+        buttonReg.setOnClickListener {
+            val email = userEmail.text.toString()
+            val login = userLogin.text.toString()
+            val pass = userPass.text.toString()
+            if (email == "" || login == "" || pass == "")
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            else {
+                val user = User(login, email, pass)
+                Toast.makeText(
+                    this, "Пользователь $login успешно зарегестрирован",
+                    Toast.LENGTH_LONG).show()
+                }
+
+
+        }
+
+
+
+        textToAuth.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
