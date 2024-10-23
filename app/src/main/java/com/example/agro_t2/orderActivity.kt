1e02b5dca5c7651b85ce1d7974ce6a6596c7110d
Voch303 <fenics302@gmail.com>
2024-10-23 11:31:58 +0300

    Заказ Окно

---
+++ app/src/main/java/com/example/agro_t2/orderActivity.kt
@@ -0,0 +1,17 @@
+package com.example.agro_t2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class orderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+
+
+    }
+}
