23732207ad33ff303ae3a8f89da51a6b3a8b9198
Alex <Alekseevnikita886@gmail.com>
2024-10-24 09:57:28 +0300

    rework User and create main menu

---
+++ app/src/main/java/com/example/agro_t2/Main_Menu.kt
@@ -0,0 +1,55 @@
+package com.example.agro_t2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.agro_t2.User
+
+
+class Main_Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_menu)
+
+        val user = User()
+        val emailMainMenu: TextView = findViewById(R.id.email_main_menu)
+        emailMainMenu.text = user.login
+        val SNP: TextView = findViewById(R.id.SNP_main_menu)
+
+        val buttonToOrder: TextView = findViewById(R.id.ToOrder_main_menu)
+        val buttonToOrderHistory: TextView = findViewById(R.id.ToOrderHistory_main_menu)
+        val buttonToRating: TextView = findViewById(R.id.ToRating_main_menu)
+        val buttonToChatController: TextView = findViewById(R.id.ToChatController_main_menu)
+        val buttonToSettings: TextView = findViewById(R.id.ToSettings_main_menu)
+
+        buttonToOrder.setOnClickListener {
+            val intent: Intent = Intent(this, orderActivity::class.java)
+            startActivity(intent)
+        }
+        buttonToSettings.setOnClickListener {
+            val intent = Intent(this, settings::class.java)
+            startActivity(intent)
+        }
+/*
+        buttonToChatController.setOnClickListener {
+            val intent = Intent(this, ChatController::class.java)
+            startActivity(intent)
+        }
+        buttonToOrderHistory.setOnClickListener {
+            val intent = Intent(this, OrderHistory::class.java)
+            startActivity(intent)
+        }
+        buttonToRating.setOnClickListener {
+            val intent = Intent(this, Rating::class.java)
+            startActivity(intent)
+        }
+*/
+
+
+    }
+}
--- app/src/main/java/com/example/agro_t2/User.kt
+++ app/src/main/java/com/example/agro_t2/User.kt
@@ -1,5 +1,8 @@
 package com.example.agro_t2
 
-class User (val login:String, val email:String, val pass:String){
-
+class User (val login:String="", val email:String="", val pass:String=""){
+    val UserLogin = login
+    val UserEmail = email
+    val UserPass = pass
 }
+
