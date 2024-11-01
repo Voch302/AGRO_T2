fb05277f11c0a5890e218bb3260ca15f60eaf677
aleks <Alekseevnikita886@gmail.com>
2024-11-01 19:19:27 +0300

    рег

---
+++ app/src/main/java/com/example/agro_t2/Registration.kt
@@ -0,0 +1,35 @@
+package com.example.agro_t2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Registration : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration)
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
